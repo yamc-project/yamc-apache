fdcdc48fc5b2f5731697defb49fbfc31f6e0f421
Tomas Vitvar <tomas@vitvar.com>
2023-06-06 22:55:27 +0200

    version fix

--- yamc_apache/__init__.py
+++ yamc_apache/__init__.py
@@ -2,3 +2,17 @@
 # @author: Tomas Vitvar, https://vitvar.com, tomas@vitvar.com
 
 from yamc_apache.providers.mod_status import ModStatusProvider
+
+from importlib.metadata import version, PackageNotFoundError
+
+
+def __getattr__(name):
+    """
+    Return the version number of the package as a lazy attribute.
+    """
+    if name == "__version__":
+        try:
+            return version("yamc-pushover")
+        except PackageNotFoundError as e:
+            return "unknown"
+    raise AttributeError(f"module {__name__} has no attribute {name}")
